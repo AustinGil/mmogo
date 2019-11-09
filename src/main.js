7c878f9d52839890b5507833e3043ec9750da412
Austin Gil <austin@stegosource.com>
2019-11-09 14:58:51 -0800

    Adding offline detection

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueOffline from "vue-offline";
+
+Vue.use(VueOffline);
 
 Vue.config.productionTip = false;
 if (process.env.NODE_ENV === "development") {
