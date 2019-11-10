2f955931ba70afadd3b8864278c416c2404ce8fc
Austin Gil <austin@stegosource.com>
2019-11-10 08:37:37 -0800

    test

--- src/views/Scan.vue
+++ src/views/Scan.vue
@@ -4,15 +4,42 @@
       Know someone with a QR code? Scan it here to add an article to your
       offline collection.
     </p>
-    <AppBtn @click="onClick" class="text-4xl">Open Scanner</AppBtn>
+
+    <AppBtn v-if="!imageCapture" @click="onClick" class="text-4xl"
+      >Open Scanner</AppBtn
+    >
+
+    <AppBtn v-else @click="takePhoto" class="text-4xl">Capture photo</AppBtn>
+
+    <img ref="img" src="" alt="" />
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    imageCapture: null
+  }),
+
   methods: {
-    onClick() {
-      alert("Just kidding. This feature hasn't been completed yet 😞.");
+    async onClick() {
+      // alert("Just kidding. This feature hasn't been completed yet 😞.");
+
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: true
+      });
+      const mediaStreamTrack = mediaStream.getVideoTracks()[0];
+      this.imageCapture = new ImageCapture(mediaStreamTrack);
+    },
+
+    async takePhoto() {
+      const imageCapture = this.imageCapture;
+      const blob = await imageCapture.takePhoto();
+
+      this.$refs.img.src = URL.createObjectURL(blob);
+      this.$refs.img.onload = () => {
+        URL.revokeObjectURL(this.src);
+      };
     }
   }
 };
